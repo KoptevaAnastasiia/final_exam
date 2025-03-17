12d990fdde496b902515ca96279c82e45b5c4d3f
KoptevaAnastasiia <150849726+KoptevaAnastasiia@users.noreply.github.com>
2025-03-17 23:48:43 +0200

    ID NAME LASTNAME VARIANT

--- final.cpp
+++ final.cpp
@@ -6,23 +6,151 @@
 #include <arpa/inet.h>
 #include <fstream>
 #include <ctime>
-
-
-#define PORT 18756
+#include <sstream>
+#define PORT 18768
 #define BUFFER_SIZE 1024
 
 
+void SendN(int clientSocket) {
+
+    std::string name="Anastasiia";
+    uint8_t tag= 0x03;
+    u_int32_t len= name.size();
+    char buffer[BUFFER_SIZE];
+    memset(buffer, 0, BUFFER_SIZE);
+
+    buffer[0] = tag;
+
+    buffer[1] = (len>> 24) & 0xFF;
+    buffer[2] = (len>> 16) & 0xFF;
+    buffer[3] = (len>> 8) & 0xFF;
+    buffer[4] = (len) & 0xFF;
+
+    memcpy(buffer+5, name.c_str(), len);
+
+ int byteSent = send(clientSocket, buffer, BUFFER_SIZE, 0);
+
+    if (byteSent == -1) {
+        std::cerr<< "xdfcgvbjhn"<< std::endl;
+    }
+    else{ std::cerr<< "11111111111"<< std::endl; }
+
+
+}
+
+void BackID(int clientSocket, uint8_t tag, u_int32_t len, const std::string &messenge) {
+    char buffer[BUFFER_SIZE];
+    memset(buffer, 0, BUFFER_SIZE);
+
+    buffer[0] = tag;
+
+    buffer[1] = (len >> 24) & 0xff;
+    buffer[2] = (len >> 16) & 0xff;
+    buffer[3] = (len >> 8) & 0xff;
+    buffer[4] = len & 0xff;
+
+    memcpy(buffer + 5, messenge.c_str(), messenge.size());
+
+    int byteSent = send(clientSocket, buffer, 5+ messenge.size(), 0);
+    if (byteSent == -1) {
+        std::cerr<< "xdfcgvbjhn"<< std::endl;
+    }
+    else {
+        std::cerr<< "11111111111"<< std::endl;
+        SendN(clientSocket);
+    }
+
+
+}
+
+
+void sendSN(int clientSocket) {
+
+    std::string name="Koptieva";
+    uint8_t tag= 0x03;
+    u_int32_t len= name.size();
+    char buffer[BUFFER_SIZE];
+    memset(buffer, 0, BUFFER_SIZE);
+
+    buffer[0] = tag;
+
+    buffer[1] = (len>> 24) & 0xFF;
+    buffer[2] = (len>> 16) & 0xFF;
+    buffer[3] = (len>> 8) & 0xFF;
+    buffer[4] = (len) & 0xFF;
+
+    memcpy(buffer+5, name.c_str(), len);
+
+    int byteSent = send(clientSocket, buffer, BUFFER_SIZE, 0);
+
+    if (byteSent == -1) {
+        std::cerr<< "xdfcgvbjhn"<< std::endl;
+    }
+    else{ std::cerr<< "1111111111K"<< std::endl; }
+
+
+}
+
+void sendVAriant(int clientSocket, const std::string &var) {
+
+    uint8_t tag = 0x12;
+    u_int32_t len = var.size();
+    char buffer[BUFFER_SIZE];
+    memset(buffer, 0, BUFFER_SIZE);
+
+    buffer[0] = tag;
+    buffer[1] = (len >> 24) & 0xFF;
+    buffer[2] = (len >> 16) & 0xFF;
+    buffer[3] = (len >> 8) & 0xFF;
+    buffer[4] = (len) & 0xFF;
+
+     memcpy(buffer + 5, var.c_str(), len);
+
+    int byteSent = send(clientSocket, buffer, 5 + len, 0);
+    if (byteSent == -1) {
+        std::cerr << "er" << std::endl;
+    } else {
+        std::cerr << "77777" << std::endl;
+    }
+}
 
 
 
+void processMessage(uint8_t tag, uint32_t length, const std::string &message, int clientSocket) {
 
 
 
+    if (tag == 0x01) {
+        if (message == "Server offline!") {
+            std::cerr << "Server offline!" << std::endl;
 
-void receiveMessages(int clientSocket) {
+        } else if (message == "Server online!") {
 
-    char buffer[BUFFER_SIZE] ;
-    std::ofstream file ("message.txt", std::ios::app);
+            std::cerr << "Server online!" << std::endl;
+        }
+    } else if (tag == 0x02) {
+        std::cout << "Session ID: " << message << std::endl;
+
+        BackID(clientSocket, tag, length, message);
+    } else if (tag == 0x03) {
+        std::cerr << "lastname: " << message << std::endl;
+        sendSN(clientSocket);
+        int var = 18;
+        sendVAriant(clientSocket, "18");
+    } else if (tag == 0x05) {
+         std::cerr << "variant: " << message << std::endl;
+
+
+    } else if (tag >= 0x10 && tag <= 0x50) {
+         std::cout << "   " << std::hex << (int)tag << ": " << message << std::dec << std::endl;
+    } else {
+        std::cout << " err tag: " << std::hex << (int)tag << std::dec << std::endl;
+    }
+}
+
+ void receiveMessages(int clientSocket) {
+    char buffer[BUFFER_SIZE];
+    std::ofstream file("message.txt", std::ios::app);
 
     if (!file) {
         std::cerr << "err" << std::endl;
@@ -30,14 +158,31 @@ void receiveMessages(int clientSocket) {
     }
 
     while (true) {
-         memset(&buffer, 0, sizeof(buffer));
+        memset(&buffer, 0, sizeof(buffer));
         int valread = recv(clientSocket, buffer, BUFFER_SIZE, 0);
         if (valread > 0) {
-            uint16_t tag = (buffer[0] << 8) | buffer[1];
-            uint16_t length = (buffer[2] << 8) | buffer[3];
+            std::cout << "byte ";
+            for (int i = 0; i < valread; ++i) {
+                std::cout << std::hex << (int)(unsigned char)buffer[i] << " ";
+            }
+            std::cout << std::dec << std::endl;
 
-            std::string message(buffer + 4, length);
 
+            uint8_t tag = buffer[0];
+            uint32_t length = (static_cast<uint8_t>(buffer[1]) << 24) |
+                              (static_cast<uint8_t>(buffer[2]) << 16) |
+                              (static_cast<uint8_t>(buffer[3]) << 8) |
+                              static_cast<uint8_t>(buffer[4]);
+
+
+            std::cout << "teg: " << std::hex << (int)tag << ", len: " << length << std::dec << std::endl;
+
+            if (length > valread - 5) {
+                std::cout << "Помилка: довжина повідомлення не відповідає кількості отриманих байтів!" << std::endl;
+                continue;
+            }
+
+            std::string message(buffer + 5, length);
             std::time_t now = std::time(nullptr);
             std::tm* localTime = std::localtime(&now);
             char timeBuffer[20];
@@ -45,7 +190,7 @@ void receiveMessages(int clientSocket) {
 
             file << "[" << timeBuffer << "] Повідомлення від сервера: " << message << std::endl;
 
-            std::cout << "Отримано повідомлення: " << message << std::endl;
+             processMessage(tag, length, message, clientSocket);
         } else {
             break;
         }
@@ -90,7 +235,6 @@ int main() {
     std::cout << "Підключено до сервера!" << std::endl;
 
 
-    std::cout << "Підключено до сервера!" << std::endl;
 
     std::thread receiveThread(receiveMessages, clientSocket);
     std::string response;
@@ -101,6 +245,7 @@ int main() {
         std::cout << "Ваша відповідь: ";
         std::getline(std::cin, response);
         if (response == "exit") break;
+
 
 
 
